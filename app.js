0493cc53b5934dccfda98a6f7656715da3528e59
EC2 Default User <ec2-user@ip-172-31-81-243.ec2.internal>
2019-10-30 19:05:05 +0000

    finishing touches

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 //express and ejs setup
 const express = require("express");
 const app = express();
+const faker = require("faker");
 app.set("view engine","ejs");
 app.use(express.static("public"));
 
+var current = 0;
+
+
 //routing
 app.get("/",function(req,res){
-    res.render("index")
+    let name = faker.name.findName();
+    current = 1;
+    res.render("index", {"current":current, "name":name});
 });
 app.get("/about",function(req,res){
-    res.render("about")
+    current = 2;
+    res.render("about", {"current":current});
 });
 app.get("/contact",function(req,res){
-    res.render("contact")
+    current = 3;
+    res.render("contact", {"current":current});
+});
+app.get("/contacts",function(req,res){
+    let contacts = [];
+    for(let i = 0; i<10; i++){
+        contacts.push(faker.name.findName())
+    }
+    current = 4;
+    res.render("contacts", {"current":current, "contacts":contacts});
 });
 
 //start server
